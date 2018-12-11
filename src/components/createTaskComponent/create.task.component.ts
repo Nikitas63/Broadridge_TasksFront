fa9d8b793015d8d0cc92b8813ce1e43d65a86969
skvortsov <skvortsov@mercurydevelopment.com>
2018-12-11 11:33:25 +0400

    Time to complete format on task creation

--- src/components/createTaskComponent/create.task.component.ts
+++ src/components/createTaskComponent/create.task.component.ts
@@ -18,13 +18,13 @@ export class CreateTaskComponent {
 
   public task: Task = new Task();
 
-  public get canSave(): boolean {
-    return this.task.Name != null && this.task.Description != null
-      && this.task.Priority > 0 && this.task.Priority < 100 && this.task.TimeToComplete > 0;
-  }
+  public daysToComplete: number = 0;
+  public hoursToComplete: number = 0;
+  public minutesToComplete: number = 0;
+  public secondsToComplete: number = 0;
 
   public get warningNeeded(): boolean {
-    return !this.canSave && this._saveAlreadyClicked;
+    return this.warningText != null && this._saveAlreadyClicked;
   }
 
   public get warningText(): string {
@@ -34,9 +34,26 @@ export class CreateTaskComponent {
     if (this.task.Description == null || this.task.Description == '') {
       return 'Task description should be filled';
     }
-    if (!this.task.Priority || this.task.Priority <= 0 || this.task.Priority > 100) {
+    if (!this.task.Priority || this.task.Priority < 0 || this.task.Priority > 100) {
       return 'Task priority should be between 1 and 100';
     }
+    if (this.daysToComplete == null || this.daysToComplete < 0 || this.daysToComplete > 30) {
+      return 'Task days to complete should be between 0 and 30';
+    }
+    if (this.hoursToComplete == null || this.hoursToComplete < 0 || this.hoursToComplete > 24) {
+      return 'Task hours to complete should be between 0 and 24';
+    }
+    if (this.minutesToComplete == null || this.minutesToComplete < 0 || this.minutesToComplete > 60) {
+      return 'Task minutes to complete should be between 0 and 60';
+    }
+    if (this.secondsToComplete == null || this.secondsToComplete < 0 || this.secondsToComplete > 60) {
+      return 'Task seconds to complete should be between 0 and 60';
+    }
+    if (this.getTimeToComplete() <= 0 ) {
+      return 'Time to complete should be greater than zero';
+    }
+
+    return null;
   }
 
   constructor(private _tasksService: TasksService,
@@ -45,11 +62,30 @@ export class CreateTaskComponent {
   public save() {
     this._saveAlreadyClicked = true;
 
-    if (this.canSave) {
+    if (this.warningText == null) {
+      this.task.TimeToComplete = this.getTimeToComplete();
       this._tasksService.createTask(this.task)
         .subscribe(createdTask => {
           this._router.navigate([`/tasks/${createdTask.Id}`]);
         });
     }
+  }
+
+  private getTimeToComplete(): number {
+    var result = 0;
+    if (this.secondsToComplete) {
+      result += this.secondsToComplete;
+    }
+    if (this.minutesToComplete) {
+      result += this.minutesToComplete * 60;
+    }
+    if (this.hoursToComplete) {
+      result += this.hoursToComplete * 60 * 60;
+    }
+    if (this.daysToComplete) {
+      result += this.daysToComplete * 60 * 60 * 24;
+    }
+
+    return result;
   }
 }
